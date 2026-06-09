50604b636f5cd492461dcf40c589ade6f4c0752f
Imane <imane3azzimani@gmail.com>
2026-06-09 21:35:24 +0200

    Add timestamp to telemetry logging

--- src/logger.c
+++ src/logger.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include "logger.h"
+#include <time.h>
 
 
 int log_telemetry_payload(const char *json_payload)
 {
+    char timestamp[32];
+    time_t now = time(NULL);
+    struct tm *tm_info = localtime(&now);
+    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_info);
+
     FILE *fp = fopen("logs/telemetry.log", "a");
 
     if(fp == NULL){
-        perror("Error: Unavailable file");
-        fclose(fp);
+        perror("Error opening log file");
         return -1;
     }
 
-    if(fprintf(fp, "%s\n", json_payload) < 0){
+    if(fprintf(fp, "%s %s\n", timestamp, json_payload) < 0){
+        fclose(fp);
         return -1;
     }
    
