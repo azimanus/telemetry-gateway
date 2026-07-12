2e96ba2ff457962f3fe2a3ec58f429b761789a7f
Imane <imane3azzimani@gmail.com>
2026-07-12 18:05:05 +0200

    cleanup of main.c file

--- src/main.c
+++ src/main.c
@@ -8,14 +8,14 @@
 #include "logger.h"
 #include "mqtt_publisher.h"
 
-volatile sig_atomic_t running_flag = 1;
-void signal_handler(int signum);
+static volatile sig_atomic_t running_flag = 1;
+static void signal_handler(int signum);
 
 int main(void)
 {
     telemetry_data_t telemetry_data;
 
-    config_t config ;
+    config_t config = {0};
 
     char buffer[256];
     int json_format_status;
@@ -23,7 +23,10 @@ int main(void)
 
     buffer_size = sizeof(buffer);
 
-    signal(SIGINT, signal_handler);
+    if(signal(SIGINT, signal_handler) == SIG_ERR){
+        printf("Error: error while handling the signal\n");
+        return -1;
+    }
 
     if(read_config(&config) < 0)
     {
@@ -31,12 +34,20 @@ int main(void)
         return -1;
     }
 
+    if(config.interval_sec <= 0 || config.mqtt_port <= 0 || config.device_id[0] == '\0'|| config.mqtt_topic[0] == '\0' || config.mqtt_host[0] == '\0'){
+        printf("Error: Invalid config parameters\n");
+        return -1;
+    }
+
     printf("Embedded Linux Telemetry Gateway \n");
     printf("Device ID   : %s\n", config.device_id);
     printf("Interval    : %d sec\n", config.interval_sec);
-    printf("Version     : 0.4\n");
+    printf("Version     : 0.5\n");
 
-    mosquitto_lib_init();
+    if(mosquitto_lib_init() != MOSQ_ERR_SUCCESS){
+        fprintf(stderr, "Failed to initialize Mosquitto library\n");
+        return -1;
+    }
 
     while(running_flag != 0)
     {
@@ -52,26 +63,16 @@ int main(void)
 
         json_format_status = format_telemetry_json(&telemetry_data, &config, buffer, buffer_size);
 
-        if(json_format_status < 0){
+        if(json_format_status != 0){
              printf("Error: json formatting error\n");
              sleep(config.interval_sec);
              continue;
         }
-        else if (json_format_status > 0) {
-            printf("Error: Buffer too small\n");
-            sleep(config.interval_sec);
-            continue;
-        
-        }
-        else{
-        
-            printf("JSON Payload: %s\n", buffer);
-        }
+       
 
         if(log_telemetry_payload(buffer) < 0){
             printf("Failed to log the telemetry\n");
-            sleep(config.interval_sec);
-            continue;
+        
         }
 
         if(mqtt_publish_payload(&config, buffer) < 0){
@@ -82,14 +83,15 @@ int main(void)
         
     }
     
-    mosquitto_lib_cleanup();
+    
     printf("Shutting down telemetry gateway...\n");
+    mosquitto_lib_cleanup();
 
 
     return 0;
 }
 
-void signal_handler(int signum){
+static void signal_handler(int signum){
     (void)signum;
     running_flag = 0;
     
